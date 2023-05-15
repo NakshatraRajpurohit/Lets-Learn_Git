e4fab2779fd00e1e55e4a723764b0f90c1601dee
nakshatra rajpurohit <nakshatrar45@gmail.com>
2023-05-16 01:29:56 +0530

    second commit

---
+++ SumOfFour.java
@@ -0,0 +1,13 @@
+public class SumOfFour{
+
+	public static void main(String args[]){
+	
+	    int a = 5;
+            int b = 6;
+            int c = 0;
+	    int d = 10;
+            System.out.println(a+b+c+d);
+
+	}
+
+}
---
+++ SumOfThree.java
@@ -0,0 +1,12 @@
+public class SumOfThree{
+
+	public static void main(String args[]){
+	
+	    int a = 5;
+            int b = 6;
+            int c = 0;
+            System.out.println(a+b+c);
+
+	}
+
+}
